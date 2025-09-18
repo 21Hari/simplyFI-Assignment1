428283f61fda7199aad7bb5be2f940309f33ea57
hari <haribalaji838@gmail.com>
2025-09-18 23:27:52 +0530

    Update with loader changes

--- src/components/userList/index.js
+++ src/components/userList/index.js
@@ -1,9 +1,11 @@
 import UserCard from '../userCard';
 import './index.css';
 import { useState, useEffect } from 'react';
+import { TailSpin } from "react-loader-spinner";
 
 const UserList = () => {
   const [users, setUsers] = useState([]);
+  const [loader, setLoader] = useState(true);
 
   useEffect(() => { gettingUserDetails() }, []);
 
@@ -16,18 +18,31 @@ const UserList = () => {
     const data = await response.json();
     console.log(data);
     setUsers(data);
+    setLoader(false);
 
   }
+
+  const renderLoader = () => (<div className="loader-container">
+    <TailSpin
+      visible={true}
+      height="60"
+      width="60"
+      color="#000000"
+      ariaLabel="tail-spin-loading"
+      radius="1"
+    />
+  </div>);
 
   return (
     <div className='user-list-container'>
       <h2 className='user-heading'>User Profiles</h2>
-      <ul className='user-list'>
+      {loader ? (renderLoader()) : (<ul className='user-list'>
         {users.map(user => (
           <UserCard key={user.id} {...user} />
         ))}
 
-      </ul>
+      </ul>)}
+
 
 
     </div>
